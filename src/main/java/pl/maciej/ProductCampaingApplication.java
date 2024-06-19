55252c7cfb78381ca17cedbdd0f726b8f3117f23
MaciejBabicki <macbabicki@gmail.com>
2024-06-19 19:21:15 +0200

    Start Project

---
+++ src/main/java/pl/maciej/ProductCampaingApplication.java
@@ -0,0 +1,13 @@
+package pl.maciej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCampaingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCampaingApplication.class, args);
+	}
+
+}
