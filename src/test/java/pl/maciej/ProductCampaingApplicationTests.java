eaa5f6ddb4e56f20b70546ffa9a6b8257a5d2599
MaciejBabicki <macbabicki@gmail.com>
2024-06-19 19:26:19 +0200

    Start Project

---
+++ src/test/java/pl/maciej/ProductCampaingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCampaingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
