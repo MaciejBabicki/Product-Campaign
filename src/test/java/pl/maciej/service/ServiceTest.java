03ce360f38b7fdab584e8adbef09d95508f54a38
MaciejBabicki <macbabicki@gmail.com>
2024-06-22 00:21:24 +0200

    Tests Happy path

---
+++ src/test/java/pl/maciej/service/ServiceTest.java
@@ -0,0 +1,94 @@
+package pl.maciej.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import pl.maciej.campaign.dto.CampaignDto;
+import pl.maciej.campaign.entity.Campaign;
+import pl.maciej.campaign.mapper.CampaignMapper;
+import pl.maciej.campaign.repository.CampaignRepository;
+import pl.maciej.campaign.service.CampaignService;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class ServiceTest {
+    Campaign campaign = new Campaign();
+    long existingId = 1;
+    long notExistingId = 2;
+    @Mock
+    private CampaignRepository repository;
+    @InjectMocks
+    private CampaignService campaignService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void test_CreateCampaign_CampaignCreated() {
+        //given
+        CampaignDto campaignDto = CampaignMapper.mapToCampaignDto(campaign);
+        when(repository.save(any(Campaign.class))).thenReturn(campaign);
+        //when
+        CampaignDto newCampaignDto = campaignService.createCampaign(campaign);
+        //then
+        assertNotNull(newCampaignDto);
+        assertEquals(newCampaignDto.getId(), campaign.getId());
+        verify(repository, times(1)).save(campaign);
+    }
+
+    @Test
+    void test_GetCampaigns_ListReturned() {
+        //given
+        Campaign campaign2 = new Campaign();
+        List<Campaign> campaigns = Arrays.asList(campaign, campaign2);
+        when(repository.findAll()).thenReturn(campaigns);
+        //when
+        List<CampaignDto> result = campaignService.getCampaigns();
+        //then
+        assertEquals(2, result.size());
+        verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void test_GetCampaign_CampaignReturned() {
+        //given
+        when(repository.findById(existingId)).thenReturn(Optional.of(campaign));
+        //when
+        CampaignDto newCampaignDto = campaignService.getCampaign(existingId);
+        //then
+        assertNotNull(newCampaignDto);
+    }
+
+    @Test
+    void test_UpdateCampaign_CampaignUpdated() {
+        //given
+        CampaignDto campaignDto = new CampaignDto();
+        when(repository.findById(existingId)).thenReturn(Optional.of(campaign));
+        //when
+        CampaignDto nerCampaignDto = campaignService.updateCampaign(existingId, campaignDto);
+        //then
+        assertNotNull(nerCampaignDto);
+    }
+
+    @Test
+    void test_DeleteCampaign_CampaignDeleted() {
+        //given
+        when(repository.findById(existingId)).thenReturn(Optional.ofNullable(campaign));
+        //when
+        campaignService.deleteCampaign(existingId);
+        //then
+        verify(repository, times(1)).findById(existingId);
+        verify(repository, times(1)).deleteById(existingId);
+    }
+}
