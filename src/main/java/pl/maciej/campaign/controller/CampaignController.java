5d69c44c51a3f172eb5d6cffbe7bd2ccd4849449
MaciejBabicki <macbabicki@gmail.com>
2024-06-24 23:05:40 +0200

    Update CampaignController endpoint for RequestMapping

--- src/main/java/pl/maciej/campaign/controller/CampaignController.java
+++ src/main/java/pl/maciej/campaign/controller/CampaignController.java
@@ -1,40 +1,43 @@
 package pl.maciej.campaign.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import pl.maciej.campaign.entity.Campaign;
 import pl.maciej.campaign.dto.CampaignDto;
+import pl.maciej.campaign.entity.Campaign;
 import pl.maciej.campaign.service.CampaignService;
 
 import java.util.List;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("campaign")
+@RequestMapping("campaigns")
 @RequiredArgsConstructor
 public class CampaignController {
     private final CampaignService service;
 
     @PostMapping
-    public CampaignDto createCampaign(@RequestBody Campaign campaign){
+    public CampaignDto createCampaign(@Valid @RequestBody Campaign campaign) {
         return service.createCampaign(campaign);
     }
 
     @GetMapping
-    public List<CampaignDto> getCampaigns(){
+    public List<CampaignDto> getCampaigns() {
         return service.getCampaigns();
     }
+
     @GetMapping("/{id}")
-    public CampaignDto getCampaign(@PathVariable("id") long id){
+    public CampaignDto getCampaign(@PathVariable("id") long id) {
         return service.getCampaign(id);
     }
+
     @PutMapping("/{id}")
-    public CampaignDto updateCampaign(@PathVariable("id") long id, @RequestBody CampaignDto campaignDto){
+    public CampaignDto updateCampaign(@PathVariable("id") long id, @RequestBody CampaignDto campaignDto) {
         return service.updateCampaign(id, campaignDto);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCampaign(@PathVariable("id") long id){
+    public void deleteCampaign(@PathVariable("id") long id) {
         service.deleteCampaign(id);
     }
 }
