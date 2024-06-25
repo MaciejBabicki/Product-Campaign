c7f0aec4df320f64ff3b1229d167cb3ca58709a1
MaciejBabicki <macbabicki@gmail.com>
2024-06-25 10:55:21 +0200

    Update ResourceNotFoundException

--- src/main/java/pl/maciej/campaign/exception/ResourceNotFoundException.java
+++ src/main/java/pl/maciej/campaign/exception/ResourceNotFoundException.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Slf4j
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(){
+    public ResourceNotFoundException() {
+        super("RCampaign with this id doestn't exist in database");
         log.info("Campaign with this id doestn't exist in database");
     }
 }
